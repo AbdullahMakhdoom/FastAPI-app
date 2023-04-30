9d59cb5160c9677ce517e12613d7eed12b817989
abdullahm1 <abdullahm@phelix.ai>
2023-04-30 18:51:12 +0500

    query paramters

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ app = FastAPI()
 async def root():
     return {"message" : "Hello World"}
 
+# Path parameters
 @app.get("/items/{items_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
@@ -33,3 +34,22 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+# Query parameters
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+@app.get("/items_query/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
